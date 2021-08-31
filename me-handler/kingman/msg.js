3e289574900c9c706ceb1a83c3812a87fc7dc974
KMKINGMAN <84540534+KMKINGMAN@users.noreply.github.com>
2021-08-31 09:39:39 -0400

    Update msg.js

--- me-handler/kingman/msg.js
+++ me-handler/kingman/msg.js
@@ -2,40 +2,8 @@
  * ------------------MeKINGMAN----------------------
  * | +962792914245          [muhammad Rafat Kurkar] |
  * --------------------------------------------------
- * |     MeCodes Discord Server Owner :)            |
- * --------------------------------------------------
-     ..                                                                                     
-    .;o:..                                                      ..:l;.                      
-    .lXX0d:..                                                ..:d0XKc.                      
-    .lXNNNX0d;..                                          ..:dOXNNNKc.                      
-    .lXNNNNNNXOo;..                                    ..:d0XNNNNNNKc.                      
-    .lXNNNNNNNNNXOo;.                               ..:d0XNNNNNNNNN0:.                      
-    .lXNNNNNNNNNNNNXOo;.                         ..:d0XNNNNNNNNNXOo;.                       
-    .lXNNNNNNNNNNNNNNNXOo,.                   ..:d0XNNNNNNNNNXOo;.                          
-    .lXNNNNNOdxKNNNNNNNNNKkl,.             ..:d0XNNNNNNNNNXOo;.                             
-    .lXNNNNNx..,lkKNNNNNNNNNKkl,.       ..:d0XNNNNNNNNNXOo;.     ....                       
-    .lXNNNNNx.   .,lkKNNNNNNNNNKkl,....:d0XNNNNNNNNNXkl,.     ...',,.                       
-    .lXNNNNNx.      .,lkKNNNNNNNNNKkxk0XNNNNNNNNNXkl;.     ...',;;;,.                       
-    .lXNNNNNx.         .,lkKNNNNNNNNNNNNNNNNNNKkl,.     ...',;;;;;;,.                       
-    .lXNNNNNx.   ...      .,lkKNNNNNNNNNNNNXkl,.     ...',,;;;;;;;,'.                       
-    .lXNNNNNx.  ..,,'..      .,lkKNNNNNNKkl,.      ..',,;,,;,;;,,'..                        
-    .lXNNNNNx.  ..,;;,,'..      .,lk00kl,.      ..',,;;;;;;,,'...                           
-    .lXNNNNNx.  ..,;;;;;,,'..      ....      ..',,;;;;;;;,'...                              
-    .lXNNNNNx.  ..,;;;;;;;;,,'..         ...',,;;;,;;,,'...       ...                       
-    .lXNNNNNx.  ..,;;;;,;;;;;;,,...    ..',;;;;;;;,,'...       ...',.                       
-    .lXNNNNNx.  ..,;;;,,,,,;;;;;;,''..',,;,;;;;;,'...       ...',;;,.                       
-    .lXNNNNNx.  ..,;;;;,...',;;;;;;;;;;;;;;;;,'...       ...',;;;;;,.                       
-      ,d0XNNNx.  ..,;;;;,.  ...',;;;;;;;;;,,'...       ...',;;;;;;;,,.                       
-      ..:dOXx.  ..,;;;;'.     ...',;;;;,'..        ...',;;;;;;;,,'..                        
-          ..:;.  ..,;;;;,..       ...'...        ...',;;;;;;;,,'...                          
-                ..,;;;;,,'...                ...',,,;;;;;,,'..                              
-                  .',;;;;;;;,'...          ...',;;;;;;;,,'..                                 
-                  ...,,,;;,;;,,'...    ...',,;;;;;;,,'..                                    
-                      ..',,;;;;;;,,'....',;;;;;;;,,'..                                       
-                        ..',,;;;;;;;,,;;;;;;;,,'..                                          
-                            ..',,;;;;;;;;;;,,'..                                             
-                              ..',;;;;;,,'..                                                
-                                  ..'''...                                                             
+ * |    KINGMANDEV Discord Server Owner :)           |
+ * --------------------------------------------------                                                        
  */
 const { MessageEmbed } = require("discord.js");
 class ME {
